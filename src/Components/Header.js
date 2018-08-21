96f4ecdcfa4103e40df306de466987f7e872504f
Wes Basinger <wbasinger@villagetechschools.org>
2018-08-21 18:44:01 -0500

    Fixed the nav link to point to home not, github project page home.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,7 +8,7 @@ import secret from '../secret.js';
 const Header = (props) => {
   return(
     <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-      <a className="navbar-brand" href="/">MathPay</a>
+      <a className="navbar-brand" href="https://wesbasinger.github.io/mathpay">MathPay</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
