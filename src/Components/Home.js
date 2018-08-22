8f3ead4daa1eb279d5d84648a38f5ff429f82d86
Wes Basinger <wbasinger@villagetechschools.org>
2018-08-22 08:24:11 -0500

    Changed the image to pull from a link rather than an image loader.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-import MainArt from './MainArt.png';
-
 const Home = (props) => {
 
     return(
       <div className="starter-template">
-        <img alt="Math Pay Logo" src={MainArt} width={500}/>
+        <img alt="Math Pay Logo" src="https://raw.githubusercontent.com/wesbasinger/mathpay/master/src/Components/MainArt.png" width={500}/>
         <h2>
           Server Status: {
             props.user ? "Connected" : "Not connected"
